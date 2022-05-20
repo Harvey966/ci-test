49e515b77be3909747d01e9473f370b222b4d143
魏航宇 <weihangyu@bytedance.com>
2022-05-20 21:06:01 +0800

    11

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ http
 
       // 创建 docker 容器
       execSync(
-        `docker run -d -p 8888:80 --name ${name}-container  ${name}-image:latest`,
+        `docker run -d -p 80:80 --name ${name}-container  ${name}-image:latest`,
         {
           stdio: "inherit",
         }
